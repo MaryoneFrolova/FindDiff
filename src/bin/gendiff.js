418edc1a9d6d43dcd73d14228d28547e42ad94f7
MaryoneFrolova <35322384+MaryoneFrolova@users.noreply.github.com>
2018-02-19 17:16:48 +1000

    add index.js

--- src/bin/gendiff.js
+++ src/bin/gendiff.js
@@ -2,7 +2,7 @@
 const program = require('commander');
 
 program
-  .version('0.0.1')
+  .version('0.0.2')
   .description('Compares two configuration files and shows a difference.')
   .arguments('<firstConfig> <secondConfig>')
   .option('-f, --format [type]', 'Output format')
